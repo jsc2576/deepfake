e0958804a1007e2262f3edb56a60548cfb78a9ee
정수찬 <scjung@mindslab.ai>
2020-02-14 15:34:32 +0900

    fix configproto

--- donghee/efficientnet_deepfake.py
+++ donghee/efficientnet_deepfake.py
@@ -15,7 +15,7 @@ import cv2
 import csv
 import os
 
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 K.set_session(tf.Session(config=config))
     
